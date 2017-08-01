f46cf175c77c790e8a832ddc5b5c773b677d8c1b
Yiwei Ni <anitani313@gmail.com>
2017-08-01 14:29:22 -0400

    add content

--- nsfwDetector/ViewController.swift
+++ nsfwDetector/ViewController.swift
@@ -9,17 +9,93 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - Properties
+     let model = nsfw()
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
+
+
+// MARK: - IBActions
+extension ViewController {
+    
+    @IBAction func pickImage(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .savedPhotosAlbum
+        present(pickerController, animated: true)
+    }
+    
+    func detectPhoto(image: UIImage) {
+        answerLabel.text = "predicting..."
+        answerLabel.textAlignment = .center
+        
+        // Convert UIImage to CVPixelBuffer
+        let buffer = image.buffer()!
+        
+        // Predict
+        guard let output = try? model.prediction(data: buffer) else {
+            fatalError("Unexpected runtime error.")
+        }
+        
+        // Grab the result from prediction
+        let proba = output.prob[1].doubleValue
+        
+        // Update the answer label
+        self.answerLabel.text = String(format: "%.6f", proba)
+    }
+}
+
+// MARK: - UIImagePickerControllerDelegate
+extension ViewController: UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        dismiss(animated: true)
+        
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Can not load image from Photos")
+        }
+        
+        photo.image = image
+        detectPhoto(image: image)
+    }
+}
+
+// MARK: - UINavigationControllerDelegate
+extension ViewController: UINavigationControllerDelegate {
+}
+
+
+// MARK: - UIImage
+extension UIImage {
+    
+    func buffer() -> CVPixelBuffer? {
+        var pixelBuffer: CVPixelBuffer? = nil
+        
+        let width = 224
+        let height = 224
+        
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        CVPixelBufferCreate(kCFAllocatorDefault, width, height, kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue:0))
+        
+        let colorspace = CGColorSpaceCreateDeviceRGB()
+        let bitmapContext = CGContext(data: CVPixelBufferGetBaseAddress(pixelBuffer!), width: width, height: height, bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: colorspace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)!
+        
+        bitmapContext.draw(self.cgImage!, in: CGRect(x: 0, y: 0, width: width, height: height))
+        
+        return pixelBuffer
+    }
+}
+
 
